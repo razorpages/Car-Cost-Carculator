199fec2c543653bc341851dee5dbbf0806ba7b1f
KungFuKevin <kevin.voet@student.nhlstenden.com>
2021-03-24 17:51:15 +0100

    Created all necessary Pages, and the Repository

---
+++ Car Cost Calculator/CarCostRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Cost_Calculator
+{
+    public class CarCostRepository
+    {
+        //All Dapper Code will be entered here.
+    }
+}
